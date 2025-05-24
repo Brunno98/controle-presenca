e6d34a520c150cf43e441a886546b9de9be66b58
Brunno Santana <brunnosantana98@gmail.com>
2025-05-24 12:56:03 -0300

    chore: incluindo arquivos do spring initializr

---
+++ src/test/java/br/com/presenca/controle/ControlePresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.presenca.controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
